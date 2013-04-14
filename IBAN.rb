e1350c78d68701677e0d08e754f4a63e9cf325a5
jayrobin <james.michael.robinson@googlemail.com>
2013-04-14 12:16:45 +0100

    Refactored IBAN into a class

--- IBAN.rb
+++ IBAN.rb
@@ -1,26 +1,15 @@
-# performs a mod-97 check on an IBAN according to ISO 7064 specification
-def is_valid?(iban)
-  # remove any spaces and set to upcase
-  iban.gsub!(" ", "").upcase!
+class IBAN
+  def self.valid?(iban)
+    # remove any spaces and set to upcase
+    iban.gsub!(" ", "").upcase!
 
-  # move the first four chars to the end of the string
-  iban = iban[4, iban.length - 1] + iban[0, 4]
+    # move the first four chars to the end of the string
+    iban = iban[4, iban.length - 1] + iban[0, 4]
 
-  # replace alpha chars with their respective digits, starting at A=10
-  iban = iban.chars.map { |char| char.match(/[A-Z]/) ? char.ord - 55 : char }
+    # replace alpha chars with their respective digits, starting at A=10
+    iban = iban.chars.map { |char| char.match(/[A-Z]/) ? char.ord - 55 : char }
 
-  # compose integer and return true if mod 97 remainder is 1
-  iban.join.to_i % 97 == 1
+    # compose integer and return true if mod 97 remainder is 1
+    iban.join.to_i % 97 == 1
+  end
 end
-
-# valid IBAN
-puts is_valid? "GB82 WEST 1234 5698 7654 32"
-
-# remove character
-puts is_valid? "GB82 WEST 124 5698 7654 32"
-
-# replace character
-puts is_valid? "GB82 WUST 1234 5698 7654 32"
-
-# swap characters
-puts is_valid? "GB82 WEST 1234 5968 7654 32"
